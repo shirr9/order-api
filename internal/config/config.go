052dee0d675dffa273232c8313c33d88f7cc0708
shirr9 <simkagaechka1@gmail.com>
2025-09-07 03:28:19 +0300

    add CacheRepository interface, Redis struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ type Config struct {
 	Env        string `yaml:"env"`
 	LogPath    string `yaml:"log_path"`
 	PostgresDb `yaml:"postgres"`
+	RedisDB    `yaml:"redis"`
 }
 
 // PostgresDb password must have correct name from .env, it must be connected with config.yaml
@@ -14,4 +15,10 @@ type PostgresDb struct {
 	Port     string `yaml:"port"`
 	DbName   string `yaml:"db_name"`
 	SSlMode  string `yaml:"sslmode" env-default:"disable"`
+}
+
+type RedisDB struct {
+	Password string `yaml:"password" env:"REDIS_PASSWORD" env-required:"true"`
+	Host     string `yaml:"host" env-default:"localhost"`
+	Port     string `yaml:"port" env:"REDIS_PORT" env-required:"true"`
 }
--- internal/storage/redis/redis.go
+++ internal/storage/redis/redis.go
@@ -2,14 +2,54 @@ package redis
 
 import (
 	"context"
+	"fmt"
 	"github.com/redis/go-redis/v9"
 	"github.com/shirr9/order-api/internal/config"
+	"time"
 )
 
-type Redis struct {
-	cache *redis.Client
+type CacheRepository interface {
+	Set(ctx context.Context, key string, value []byte, ttl time.Duration) error
+	Get(ctx context.Context, key string) ([]byte, error)
+	Delete(ctx context.Context, key string) error
+	Close() error
 }
 
-func NewRedis(ctx *context.Context, cfg *config.Config) *Redis {
+type Redis struct {
+	client *redis.Client
+}
 
+func NewRedis(ctx context.Context, cfg *config.Config) (*Redis, error) {
+	// url = "redis://<user>:<pass>@localhost:6379/<db>"
+	adr := fmt.Sprintf("%s:%s", cfg.RedisDB.Host, cfg.RedisDB.Port)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     adr,
+		Password: cfg.RedisDB.Password,
+		DB:       0,
+	})
+
+	if _, err := rdb.Ping(ctx).Result(); err != nil {
+		return nil, err
+	}
+	return &Redis{client: rdb}, nil
+}
+
+func (r Redis) Set(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return r.client.Set(ctx, key, value, ttl).Err()
+}
+
+func (r Redis) Get(ctx context.Context, key string) ([]byte, error) {
+	data, err := r.client.Get(ctx, key).Bytes()
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (r Redis) Delete(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
+}
+
+func (r Redis) Close() error {
+	return r.client.Close()
 }
